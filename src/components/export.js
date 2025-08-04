21bb53a49d215d7350f60177b3ececc3cacc7ef6
Alexandre <al.moreau@thomyris.com>
2025-08-04 14:53:11 +0200

    fix(import): components name

--- src/components/export.js
+++ src/components/export.js
@@ -1,6 +1,6 @@
-import Header from './Header'
-import Login from './Login'
-import Footer from './Footer'
-import PrivateRoute from './PrivateRoute'
+import Header from './Header.jsx'
+import Login from './Login.jsx'
+import Footer from './Footer.jsx'
+import PrivateRoute from './PrivateRoute.jsx'
 
 export {Header, Login, Footer, PrivateRoute}
