d9f0a971a9dc4ede7f0f620e0943f7e93a4d872d
Alexandre <moreaualexandre2002@gmail.com>
2025-11-11 00:19:58 +0100

    style(footer): fix social media link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,10 @@
 // src/components/Footer.jsx
 import Logo from '../asset/Logo.webp'
 import { Link, NavLink } from 'react-router-dom'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export default function Footer() {
   return (
-    <HelmetProvider>
-      <footer className="footer">
-        <Helmet>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-            integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-        </Helmet>
+    <footer className="footer">
         <div className="footer__left">
           <NavLink to="/Mentions-legales" className="footer__link">
             Mentions légales
@@ -29,7 +18,8 @@ export default function Footer() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <i className="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-facebook" aria-hidden="true"></i>
+            <span className="sr-only">Facebook</span>
           </a>
           <a
             href="https://instagram.com"
@@ -37,7 +27,17 @@ export default function Footer() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <i className="fa-brands fa-instagram"></i>
+            <i className="fa-brands fa-instagram" aria-hidden="true"></i>
+            <span className="sr-only">Instagram</span>
+          </a>
+          <a
+            href="https://twitter.com"
+            className="footer__social-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-brands fa-twitter" aria-hidden="true"></i>
+            <span className="sr-only">Twitter</span>
           </a>
         </div>
 
@@ -46,20 +46,11 @@ export default function Footer() {
         </Link>
 
         <div className="footer__right">
-          <a
-            href="https://twitter.com"
-            className="footer__social-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fa-brands fa-twitter"></i>
-          </a>
           <p className="footer__copyright-text">
             Copyright © 2023 Snoroc. Tous droits réservés. Tous les
             enregistrements musicaux sont protégés par le droit d'auteur.
           </p>
         </div>
       </footer>
-    </HelmetProvider>
   )
 }
